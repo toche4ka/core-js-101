62ee2d5b799ddccf17d59a4b5e85dcfedd66d22e
Anna Tochk <anitochka1105@gmail.com>
2024-10-28 01:04:05 +0500

    feat: add solution

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -206,8 +206,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -225,8 +230,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber( value, def ) {
+  return;
 }
 
 module.exports = {
